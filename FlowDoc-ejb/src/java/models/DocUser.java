b3fe85ef8f5659b1e0c3dc91b2de2d4526a43575
Makarov Sergey <makar1488@gmail.com>
2016-05-02 12:01:57 +0300

    Обновлена объектная модель Таблица пользователей переименована из "document_users" в "users" Удалена лишняя таблица, генерируемая по объектной модели

--- FlowDoc-ejb/src/java/models/DocUser.java
+++ FlowDoc-ejb/src/java/models/DocUser.java
@@ -14,7 +14,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
-@Table(name = "document_users")
+@Table(name = "users")
 public class DocUser implements Serializable {
 
     @Id
--- FlowDoc-ejb/src/java/models/Document.java
+++ FlowDoc-ejb/src/java/models/Document.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
@@ -39,6 +40,7 @@ public class Document implements Serializable {
     @ManyToOne
     private DocUser approver;
     @OneToMany
+    @JoinColumn(name = "DOCUMENT_ID")
     private List<DocumentHistory> historys;
 
     public int getId() {
